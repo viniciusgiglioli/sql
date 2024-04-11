b74440f6f6a554062d559023cf8765c09d4f0e7c
viniciusgiglioli <81700940+viniciusgiglioli@users.noreply.github.com>
2024-04-11 12:47:39 -0300

    Limpeza de caracter especial via SQL para marketing cloud
    !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~¡¢£¤¥¦§̈¨©ª«¬®̄¯°±²³́´μ¶·̧¸¹º»1⁄41⁄23⁄4¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿ

---
+++ Caracter especial 2.sql
@@ -0,0 +1,65 @@
+SELECT
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+REPLACE(
+sua_mensagem,
+CHAR(33), ''),
+CHAR(34), ''),
+CHAR(35), ''),
+CHAR(36), ''),
+CHAR(37), ''),
+CHAR(38), ''),
+CHAR(39), ''),
+CHAR(40), ''),
+CHAR(41), ''),
+CHAR(42), ''),
+CHAR(43), ''),
+CHAR(44), ''),
+CHAR(45), ''),
+CHAR(46), ''),
+CHAR(47), ''),
+CHAR(58), ''),
+CHAR(59), ''),
+CHAR(60), ''),
+CHAR(61), ''),
+CHAR(62), ''),
+CHAR(63), ''),
+CHAR(91), ''),
+CHAR(93), ''),
+CHAR(94), ''),
+CHAR(95), ''),
+CHAR(96), ''),
+CHAR(123), ''),
+CHAR(124), ''),
+CHAR(125), ''),
+CHAR(126), '')
+
+    
+from LP_MERCADO_LIVRE_Disparo_Copy
